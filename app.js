0715b75979f76d6aeeba094cf823b163920d21a8
=rfkih23 <=rifkihidayat23@yahoo.com>
2022-01-16 21:18:43 +0700

    delete

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const server = http.createServer((req, res)=>{
 
     const todosBuffer = fs.readFileSync('./data/todos.json');
     const todos = JSON.parse(todosBuffer)
+    const query = url.parse(req.url, true).query;
 
     switch (req.method) {
         case "GET":
@@ -38,11 +39,36 @@ const server = http.createServer((req, res)=>{
         });
 
         case "PUT" :
-            const query = url.parse(req.url, true).query;
-            console.log(query)
-        
+            
+            
+            req.on("data", (body) =>{
+                const bodyParsed = JSON.parse(body);
+          
+          const selectedIndex = todos.findIndex(
+            (todo) => todo.id === parseInt(query.id)
+          );
+          
+          todos[selectedIndex].isDone = bodyParsed.isDone;
+
+          const todosString = JSON.stringify(todos);
+          fs.writeFileSync("./data/todos.json", todosString);
+
+
+            }).on("end", () => {
+                res.writeHead(200, header);
+                res.end("todo berhasil diubah")
+            });
+
+           
+        case "DELETE" :
+
+            const id = parseInt(query.id);
+            const filteredTodos = todos.filter((todo) => todo.id !==id)
+            fs.writeFileSync('./data/todos.json', JSON.stringify(filteredTodos));
     
-       
+            res.writeHead(200, header);
+            res.end("todo berhasil dihapus")
+
     }
 
 
